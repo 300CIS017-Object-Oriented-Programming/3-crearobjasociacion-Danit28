74c5af1b70aef0fd889b7527cd7356cf8c874ad1
DaniTellez <danit2002@javerianacali.edu.co>
2024-02-11 22:18:03 -0500

    Nuevo constructor en propietario

--- Propietario.h
+++ Propietario.h
@@ -14,6 +14,7 @@ private:
     int edad;
 public:
     Propietario() = default; //Agrega constructor por defecto sin cuerpo
+    Propietario(std::string nombre);
     Propietario(std::string nombre, std::string docIdentidad);
     void mostrarInfo();
     std::string getNombre();
