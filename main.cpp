d105011526a0b17abda217bb31d70432c663606d
DaniTellez <danit2002@javerianacali.edu.co>
2024-02-11 22:23:51 -0500

    Probar constructor de Perro

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ int main() {
 
     // Instanciar
     Perro firulais;
+    Perro *maxPerro = new Perro("Max",2,"Labrador","Mediano","Cafe");
     firulais.setNombre("Firulais");
     firulais.setColor("Negro");
     firulais.setEdad(5);
     firulais.setTamanio("Grande");
     firulais.setRaza("Mastin napolitano");
+    maxPerro->ladrar();
 
     //Agregar un nuevo pPropietario a Firulais
     firulais.agregarPropietario("Carlos Zapata", "102842313");
